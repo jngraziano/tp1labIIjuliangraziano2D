e061fcff5e5ef452b61335eb36ad05bf4c8b6224
jngraziano <jngraziano@gmail.com>
2018-05-18 04:34:23 -0300

    Tp2 Terminado

---
+++ TP-02/Entidades/Dulce.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string codbar, ConsoleColor color)
+        {
+            base._codigoDeBarras = codbar;
+            base._marca = marca;
+            base._colorPrimarioEmpaque = color;
+        }
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}" , this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
