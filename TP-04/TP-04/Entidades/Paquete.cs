9b2fd181a9ab50fd9200ced0d80b52361149c47d
jngraziano <jngraziano@gmail.com>
2018-06-09 21:33:56 -0300

    Tp - 04 act

--- TP-04/TP-04/Entidades/Paquete.cs
+++ TP-04/TP-04/Entidades/Paquete.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    class Paquete
+    class Paquete:IMostrar
     {
         enum EEstado
         {
--- TP-04/TP-04/MainCorreo/Form1.cs
+++ TP-04/TP-04/MainCorreo/Form1.cs
@@ -16,5 +16,10 @@ namespace MainCorreo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
